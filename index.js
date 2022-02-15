0be913132957cb144d4eaa465a0042b650a7241d
Gaurav Shrestha <gaurav.shrestha@practifi.com>
2022-02-15 20:15:22 +1100

    Test 1

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const app = express();
 const product = require("./api/product");
-
+const nunjucks = require('nunjucks');
+const indexRouter = require('./api/index');
+nunjucks.configure('./views/', {
+    autoescape: true,
+    express: app,
+  });
 app.use(express.json({ extended: false}));
-
 app.use("/api/product", product);
+app.use('/', indexRouter);
+app.set('view engine', 'njk');
 
 const PORT = process.env.PORT || 3000;
 
